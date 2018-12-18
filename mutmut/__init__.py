394176557e8090d12ae1834dc005b7652488706e
Nathan Klapstein <nklapste@ualberta.ca>
2018-12-17 17:32:55 -0700

    adding better docs

--- mutmut/__init__.py
+++ mutmut/__init__.py
@@ -5,6 +5,12 @@
 
 Mutmut is a mutation testing system for Python 2 and 3, with a strong focus
 on ease of use.
+
+Scripts:
+ + :mod:`.__main__` - main entry point script
+Modules:
+ + :mod:`.mutators` - mutant definitions
+ + :mod:`.runner` - mutation test runner
 """
 
 __version__ = '1.1.0'
